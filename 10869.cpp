a0953ce5249aedb52cf59927c3421aed0fcc9731
seonm9119 <seonm9119@gmail.com>
2023-11-20 20:02:35 +0900

    사칙연산

---
+++ 10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d", a%b);
+
+    return 0;
+}
