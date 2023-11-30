9e1ce5c17f3acee5b69b6cf86809974c4ec62f40
seonm9119 <seonm9119@gmail.com>
2023-12-01 00:37:47 +0900

    코딩은 체육과목 입니다

---
+++ 25314.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    string str = "";
+
+    for(int i=0; i<n/4; i++)
+        str += "long ";
+    
+    cout << str << "int";
+
+    return 0;
+}
